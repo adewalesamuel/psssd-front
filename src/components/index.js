bdea8bf3940e4cae88dc65de0c8cf241746f4f19
adewalesamuel <samroberval@gmail.com>
2024-01-31 22:38:53 +0000

    project init

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import { CountryForm } from './forms/CountryForm';
+import { CategoryForm } from './forms/CategoryForm';
+import { UserForm } from './forms/UserForm';
+import { ProductForm } from './forms/ProductForm';
+import { OrderForm } from './forms/OrderForm';
+import { PermissionForm } from './forms/PermissionForm';
+import { RoleForm } from './forms/RoleForm';
+import { AdminForm } from './forms/AdminForm';
+import { EbookForm } from './forms/EbookForm';
+
+export const Components = {
+    CountryForm,
+	CategoryForm,
+	UserForm,
+	ProductForm,
+	OrderForm,
+	PermissionForm,
+	RoleForm,
+	AdminForm,
+	EbookForm,
+	
+}

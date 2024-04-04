58ed5d9aef616c50fbaf8b14e2f671daa98bad08
Samuel Adewale <samueladewale@192.168.1.12>
2024-04-04 10:40:16 +0000

    Password form field added

--- src/components/forms/UserForm.jsx
+++ src/components/forms/UserForm.jsx
@@ -43,21 +43,12 @@ export function UserForm(props) {
                         disabled={props.isDisabled} readOnly={true} required/>
                     </div>
                 </div>
-                <div className='col-12'>
-                    <div className='form-group'>
-                        <label htmlFor='fullname'>Nom</label>
-                        <input className='form-control' type='text' id='fullname' name='fullname' 
-                        placeholder='Nom' value={props.useUser.fullname ?? ''}
-                        disabled={props.isDisabled} onChange={ e => 
-                            props.useUser.setFullname(e.target.value) ?? null} required/>
-                    </div>
-                </div>
 				<div className='col-12'>
                     <div className='form-group'>
                         <label htmlFor='email'>Login</label>
                         <input className='form-control' type='text' id='email' name='email' 
                         placeholder='Login' value={props.useUser.email ?? ''}
-                        disabled={props.isDisabled} onChange={ e => 
+                        disabled={props.isDisabled} readOnly onChange={ e => 
                             props.useUser.setEmail(e.target.value) ?? null} required/>
                     </div>
                 </div>
@@ -67,6 +58,33 @@ export function UserForm(props) {
                         <input className='form-control' type='text' id='phone_number' name='phone_number' 
                         placeholder='Numéro de téléphone' value={props.useUser.user?.phone_number ?? ''}
                         disabled={props.isDisabled} readOnly={true} required/>
+                    </div>
+                </div>
+                <div className='col-12'>
+                    <div className='form-group'>
+                        <label htmlFor='country_id'>Pays</label>
+                        <select
+                            className='select2 form-control'
+                            id='country_id'
+                            name='country_id'
+                            value={props.useUser.country_id ?? ''}
+                            disabled={true}
+                            onChange={e => props.useUser.setCountry_id(e.target.value) ?? null}
+                        >
+                            <option hidden>Pays de residence</option>
+                            {props.countries.map((country,index) => {
+                                return (<option value={country.id} key={index}>{country.name}</option>)
+                            })}
+                        </select>
+                    </div>
+                </div>
+                <div className='col-12'>
+                    <div className='form-group'>
+                        <label htmlFor='fullname'>Nom</label>
+                        <input className='form-control' type='text' id='fullname' name='fullname' 
+                        placeholder='Nom' value={props.useUser.fullname ?? ''}
+                        disabled={props.isDisabled} onChange={ e => 
+                            props.useUser.setFullname(e.target.value) ?? null} required/>
                     </div>
                 </div>
 				<div className='col-12'>
@@ -105,22 +123,13 @@ export function UserForm(props) {
                             props.useUser.setShop_name(e.target.value) ?? null} required/>
                     </div>
                 </div>
-				<div className='col-12'>
+                <div className='col-12'>
                     <div className='form-group'>
-                        <label htmlFor='country_id'>Pays</label>
-                        <select
-                            className='select2 form-control'
-                            id='country_id'
-                            name='country_id'
-                            value={props.useUser.country_id ?? ''}
-                            disabled={props.isDisabled}
-                            onChange={e => props.useUser.setCountry_id(e.target.value) ?? null}
-                        >
-                            <option hidden>Pays de residence</option>
-                            {props.countries.map((country,index) => {
-                                return (<option value={country.id} key={index}>{country.name}</option>)
-                            })}
-                        </select>
+                        <label htmlFor='shop_name'>Mot de passe</label>
+                        <input className='form-control' type='password' id='password' name='password' 
+                        placeholder='Mot de passe' value={props.useUser.password ?? ''}
+                        disabled={props.isDisabled} onChange={ e => 
+                            props.useUser.setPassword(e.target.value) ?? null} required/>
                     </div>
                 </div>
 				
